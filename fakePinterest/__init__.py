748d65091d2506e4c5b54ccbece843adec895198
GustavoFerrera <gustavo.ferrera.nasc@gmail.com>
2023-12-18 10:02:05 -0300

    atualizando banco

--- fakePinterest/__init__.py
+++ fakePinterest/__init__.py
@@ -6,7 +6,7 @@ import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "306bc4fe466e1cf63a9c91d229dd9d939"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
